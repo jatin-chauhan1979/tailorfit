3ec7fb2ed3990338ff19fc66744fc7c5293c40f6
jatin-chauhan1979 <jatin.chauhan1979@gmail.com>
2026-09-25 10:38:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
 from flask import Flask, request, jsonify
 from engine.master_engine import measure
+from flask_cors import CORS   #for CORS
 import os
 
 app = Flask(__name__)
+#CORS(app)
+
+CORS(
+    app,
+    origins=[
+        "http://localhost:5173",
+        "http://localhost",
+        "https://localhost",
+    ]
+)
 
 @app.route("/measure", methods=["POST"])
 def measure_api():
